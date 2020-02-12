2a32e755b011197e54ad6d00c6e397df16f1561a
Rigaux <t.rigaux@uleth.ca>
2020-02-12 12:30:35 -0700

    Added dynamic timelining basics and adding start and end times.

--- www/js/app.js
+++ www/js/app.js
@@ -8,26 +8,41 @@ let app = new Framework7({
         },
         {
             path: '/timeline/',
-            url: 'timeline.html'
+            url: 'timeline.html',
+            keepAlive: false
         }
     ]
 });
 
 let mainView = app.views.create('.view-main');
 
+
+function SubmitForm() {
+    $('input[type="time"]').on('change', function(e) {
+        var empty = false;
+        $('form[name="time"] > div > div > div > input[type="time"]').each(function() {
+            if ($(this).val() == '') {
+                empty = true;
+            }
+        });
+        
+        if (empty)
+            $('#submit').attr('href', '');
+        else
+            $('#submit').attr('href', '/timeline/');
+    });
+    $('#submit').on('click', function(e) {
+        e.preventDefault();
+        var data = app.form.convertToData('form[name="time"]');
+        start = data["start"]; end = data["end"];
+    });
+}
+
 $(function() {
+    SubmitForm();
 });
 
+$(document).on('page:beforein', '.page[data-name="home"]', function(e) {
+    SubmitForm();
+});
 
-
-//$('#submit').on('click', $('.form-ajax-submit').submit());
-/*$('.form-ajax-submit').on('submit', function(e){
-    $.ajax({
-        type: "post",
-        url: "/timeline/",
-        data: $(this).serialize(),
-        success: function(data) {
-            alert('Submitted!');
-        }
-    });
-});*/
--- www/js/timeline.js
+++ www/js/timeline.js
@@ -14,34 +14,61 @@ class Timeline {
 
         var date = FormatTimelineDate(today.getDay(), today.getMonth());
 
-        this.timeline += this.AddTime('9:00 am', 'Start Work', date);
-        this.timeline += this.AddTime('10:15 am', 'Take a Break')
+        var diff = this.TimeToInt(end) - this.TimeToInt(start);
+
+
+        this.timeline += this.AddTime(this.TimeToInt(start), 'Start Work', date);
+        var last_break = 0;
+        for(var i = 1; i < diff/100; i++)
+        {
+            last_break += (this.TimeToInt(end) / (diff/100));
+            if(last_break > this.TimeToInt(start) && last_break < this.TimeToInt(end))
+            {
+                this.timeline+= this.AddTime(last_break);
+                this.timeline+= this.AddTime(last_break + 15, "Take a break");
+                this.timeline+= this.AddTime(last_break + 35, "Back to work");
+            }
+        }
+        this.timeline += this.AddTime(this.TimeToInt(end), 'End Work');
 
         this.timeline += '</div>';
         $('#timeline').html(this.timeline);
+    }
+
+    TimeToInt(time) {
+        var hoursMinutes = time.split(/[.:]/);
+        var hours = parseInt(hoursMinutes[0], 10) * 100;
+        var minutes = hoursMinutes[1] ? parseInt(hoursMinutes[1], 10) : 0;
+        return hours + minutes;
     }
 
     CalculateBreaks(start, end) {
     }
 
     AddTime(time, task=null, date=null) {
+        var hours = parseInt(time/100);
+        var min = parseInt(time/100 - hours);
+        var formatTime = String((hours < 10) ? hours = "0" + hours : hours) + ':' + String((min < 10) ? min = "0" + min : min);
         return `<div class="timeline-item timeline-item-right">
             <div class="timeline-item-date">` + (date !== null ? date : ``) + `</div>
             <div class="timeline-item-divider"></div>
             <div class="timeline-item-content">` + (task === null ? 
-            `<div class="timeline-item-time">` + time + `</div>` :
+            `<div class="timeline-item-time">` + formatTime + `</div>` :
             `<div class="timeline-item-inner">
-                <div class="timeline-item-time">` + time + `</div>
+                <div class="timeline-item-time">` + formatTime + `</div>
                 <div class="timeline-item-title">` + task + `</div>
             </div>`) + `</div></div>`;
     }
 
     CheckIfBreakTimer() {
-
+        var today = Date.now();
+        alert(today.getTime());
     }
 };
 
-$(document).on('page:init', '.page[data-name="timeline"]', function(e){
-    let timeline = new Timeline();
+
+var start, end;
+$(document).on('page:afterin', '.page[data-name="timeline"]', function(e){
+    let timeline = new Timeline(start, end);
     let timer = setInterval(() => timeline.CheckIfBreakTimer(), 10000);
 });
